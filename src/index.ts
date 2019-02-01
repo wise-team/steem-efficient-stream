4c4cf1b9d71da39c28e0d5551de2fdb7e7177f16
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-02-01 19:42:28 +0100

    fix: export classes

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { SteemAdapter } from "./blockchain/SteemAdapter";
+import { SteemAdapterImpl } from "./blockchain/SteemAdapterImpl";
+import { BlockchainConfig } from "./blockchain/BlockchainConfig";
+
+import { UnifiedSteemTransaction } from "./blockchain/UnifiedSteemTransaction";
+import { SteemOperationNumber } from "./blockchain/SteemOperationNumber";
+
+export {
+    SteemAdapterImpl as SteemAdapter,
+    BlockchainConfig,
+    UnifiedSteemTransaction,
+    SteemOperationNumber,
+};
