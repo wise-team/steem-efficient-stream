6138cc7309a11d30e3a93b0c8315d23d9791bd8c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-02-21 16:12:45 +0100

    ci: fix branch in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,7 +3,7 @@ set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # root dir
 cd "${DIR}"
 
-
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [ "${BRANCH}" != "master" ]; then echo "Error: Branch must be master" && exit 1; fi
 
 npm version -i
