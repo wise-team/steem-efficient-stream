4e61334a1c53d5f9276d293b6e8af5b5ea639571
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-02-21 15:28:01 +0100

    ci: build only branches, do not automatically version

--- scripts/ci/publish-development.sh
+++ scripts/ci/publish-development.sh
@@ -4,6 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.." # root dir
 cd "${DIR}"
 
 git checkout "${TRAVIS_BRANCH}"
-npm version prerelease --preid=alpha
 npm whoami
 npm publish --tag alpha
